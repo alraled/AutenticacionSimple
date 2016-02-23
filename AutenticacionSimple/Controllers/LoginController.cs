2bfdc2d0e05d1a5fe348b4510504d4f200c18038
alvaro <alraled@hotmail.es>
2016-02-23 09:47:42 +0100

    AutenticacionSimple

---
+++ AutenticacionSimple/Controllers/LoginController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using System.Web.UI.WebControls;
+using AutenticacionSimple.Models;
+
+namespace AutenticacionSimple.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Index(Usuario model)
+        {
+            //var esta = HttpContext.User.Identity;
+
+            if (model.Login == model.Password)
+            {
+                var iden = new GenericIdentity(model.Login);
+                var prin = new GenericPrincipal(iden,new []{"Admin"});
+
+
+
+                Thread.CurrentPrincipal = prin;
+                FormsAuthentication.SetAuthCookie(model.Login,false);
+                return RedirectToAction("Index", "Home");
+
+            }
+
+            ModelState.AddModelError("Err",
+            "Autenticacion incorrecta");
+            return View(model);
+        }
+
+
+    }
+}
---
+++ AutenticacionSimple/Models/Usuario.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.UI.WebControls;
+
+namespace AutenticacionSimple.Models
+{
+    public class Usuario
+    {
+
+        [DisplayName("Nombre de Usuario")]
+        public String Login { get; set; }
+
+        [DisplayName("Contraseña")]
+        public String Password { get; set; }
+    
+        }
+    }
+
